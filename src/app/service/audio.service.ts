17c31a10dce85daa95844c4e31edfa3491d08b4b
tanvir bahtti <tanvirbhatti682@gmail.com>
2020-12-23 20:22:35 +0530

    gg

--- src/app/service/audio.service.ts
+++ src/app/service/audio.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Artist} from "./artist.module"
 import { AngularFirestore, AngularFirestoreModule } from '@angular/fire/firestore';
-import {AngularFireStorage,AngularFireStorageReference,AngularFireUploadTask} from '@angular/fire/storage'
+// import {AngularFireStorage,AngularFireStorageReference,AngularFireUploadTask} from '@angular/fire/storage'
 
 
 @Injectable({
@@ -9,21 +9,13 @@ import {AngularFireStorage,AngularFireStorageReference,AngularFireUploadTask} fr
 })
 export class AudioService {
 
-  uploadProgress
 
-  constructor(private firestore: AngularFirestore,
-     private storage:AngularFireStorage,
-     private ref :AngularFireStorageReference,
-     private task : AngularFireUploadTask
-     ) { }
+  constructor(private firestore: AngularFirestore) { }
   createartist(artist: Artist){
-    return this.firestore.collection('Artists').add(artist);}
+    return this.firestore.collection('Artists').add(artist);
+  }
 
-    uploadsong(event) {
-      const randomId = Math.random().toString(36).substring(2);
-      this.ref = this.storage.ref(randomId);
-      this.task = this.ref.put(event.target.files[0]);
-      this.uploadProgress = this.task.snapshotChanges()
-        .pipe(map(s => (s.bytesTransferred / s.totalBytes) * 100));
-    }
+    // uploadsong() {
+      
+    // }
 }
